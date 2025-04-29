76e862bcf174c7ec521c372cc09b794dc9e8995f
KoJieQi <122100368+KoJieQi@users.noreply.github.com>
2025-04-29 14:14:21 +0800

    Update cam_detection.py
    more camera

--- detect/cam_detection.py
+++ detect/cam_detection.py
@@ -1,12 +1,12 @@
 import cv2
 
-def list_available_cameras():
+def list_available_cameras(max_cameras=2):
     cv2.setLogLevel(0)  # Silence OpenCV error messages
     available_cameras = []
-    
-    cap = cv2.VideoCapture(0)
-    if cap is not None and cap.isOpened():
-        available_cameras.append("Webcam 0")
-        cap.release()
+    for i in range (max_cameras):
+        cap = cv2.VideoCapture(i)
+        if cap is not None and cap.isOpened():
+            available_cameras.append("Webcam "+str(i))
+            cap.release()
 
     return available_cameras
